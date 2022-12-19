ab8152928470a3f54f0d939d547ddfd5acc95a1f
MarcasRealAccount <thetremine.lumicks@gmail.com>
2022-12-19 22:49:33 +0100

    Add patch generator, remove unnecessary indices

---
+++ genPatches.py
@@ -0,0 +1,44 @@
+import os
+import datetime
+
+for pkg in os.listdir("newPatches"):
+	path      = "newPatches/" + pkg
+	repo      = ""
+	branch    = ""
+	committer = ""
+	with open(path + "/info.txt", mode="r") as f:
+		repo      = f.readline().strip()
+		branch    = f.readline().strip()
+		committer = f.readline().strip()
+	print(f"{repo}\n{branch}\n{committer}")
+	os.system(f"git clone --depth=1 --branch={branch} {repo} pkgs/{pkg}/")
+	latestCommit = ""
+	with open(f"pkgs/{pkg}/.git/HEAD", mode="r") as f:
+		latestCommit = f.readline().strip()
+	now             = datetime.datetime.now()
+	patch           = f"From {latestCommit} Mon Sep 17 00:00:00 2001\nFrom: {committer}\nDate: {now:%a, %d %b %Y %H:%M:%S %z}\nSubject: [PATCH] Add premake scripts :>\n\n---\n"
+	patchHeader     = ""
+	patchHeaderEnd  = ""
+	patchBody       = ""
+	totalFileCount  = 0
+	totalInsertions = 0
+	for f in os.listdir(path):
+		if f == "info.txt":
+			continue
+		plusses     = ""
+		fileContent = ""
+		lineCount   = 0
+		with open(path + "/" + f) as ff:
+			totalFileCount += 1
+			for line in ff:
+				fileContent     += '+' + line.rstrip() + '\n'
+				lineCount       += 1
+				plusses         += "+"
+				totalInsertions += 1
+		patchHeader    += f" {f} | {lineCount} {plusses}\n"
+		patchHeaderEnd += f" create mode 100644 {f}\n"
+		patchBody      += f"diff --git a/{f} b/{f}\nnew file mode 100644\n--- /dev/null\n+++ b/{f}\n@@ -1,0 +1,{lineCount} @@\n{fileContent}\\ No newline at end of file\n"
+	patch += f"{patchHeader} {totalFileCount} files changed, {totalInsertions} insertions(+)\n{patchHeaderEnd}\n{patchBody}"
+	patch += "--\n2.38.1.windows.1"
+	with open(f"patches/pkg-{pkg}.patch", mode="w") as f:
+		f.write(patch)
